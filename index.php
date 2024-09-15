b9025a8dd6c66067838bb60795ceb7f1c268c2eb
JulyellenS <julyellensantos1818@gmail.com>
2024-09-15 17:27:21 -0300

    Passando a rota da listagem no arquivo index

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+require_once __DIR__ . '/app/control/PessoaList.php';
 
-require 'vendor/autoload.php';
+$action = isset($_GET['action']) ? $_GET['action'] : 'listar';
+
+$controller = new PessoaList();
+
+switch ($action) {
+    case 'editar':
+        $controller->editar();
+        break;
+    case 'excluir':
+        $controller->excluir();
+        break;
+    default:
+        $controller->listar();
+        break;
+}
