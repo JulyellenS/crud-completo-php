fda374dfca479c0a695b0876eecd331941187f17
JulyellenS <julyellensantos1818@gmail.com>
2024-09-14 23:53:51 -0300

    Corrigindo chamada da senha do banco de dados através do arquivo .env

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -1,5 +1,10 @@
 <?php
 
+require '../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 class Conexao
 {
     private $host = 'localhost';
@@ -11,7 +16,7 @@ class Conexao
 
     public function __construct()
     {
-        $this->password = getenv('SENHA_BANCO');
+        $this->password = $_ENV['SENHA_BANCO'];
     }
 
     public function conectar()
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
 
 require 'vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
