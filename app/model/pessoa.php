9ef313760a9c6162c508245a6f9f22237e0c38c9
JulyellenS <julyellensantos1818@gmail.com>
2024-09-14 21:18:00 -0300

    Finalizando model de pessoa

--- app/model/pessoa.php
+++ app/model/pessoa.php
@@ -15,15 +15,22 @@ class Pessoa
     private $nm_estado;
     private $nm_cidade;
 
-    public function __construct($id_pessoa = null, $nm_pessoa = null, $nu_cpf = null, $nu_registro = null, $dt_nascimento = null)
+    public function __construct($id_pessoa = null,$nm_pessoa = null,$nu_cpf = null,$nu_registro = null,$dt_nascimento = null,$sg_orgaoexpedidor = null,$nu_cep = null,$nm_rua = null,$nm_bairro = null,$ds_complemento = null,$nu_endereco = null,$nm_estado = null,$nm_cidade = null)
     {
         $this->id_pessoa = $id_pessoa;
         $this->nm_pessoa = $nm_pessoa;
         $this->nu_cpf = $nu_cpf;
         $this->nu_registro = $nu_registro;
         $this->dt_nascimento = $dt_nascimento;
+        $this->sg_orgaoexpedidor = $sg_orgaoexpedidor;
+        $this->nu_cep = $nu_cep;
+        $this->nm_rua = $nm_rua;
+        $this->nm_bairro = $nm_bairro;
+        $this->ds_complemento = $ds_complemento;
+        $this->nu_endereco = $nu_endereco;
+        $this->nm_estado = $nm_estado;
+        $this->nm_cidade = $nm_cidade;
     }
-
 
     public function getIdPessoa(): int
     {
@@ -34,15 +41,124 @@ class Pessoa
     {
         $this->id_pessoa = $id_pessoa;
     }
-    
-	public function get_ds_complemento()
-    {
-		return $this->ds_complemento;
-	}
 
-	public function set_ds_complemento($ds_complemento)
+    public function getNmPessoa(): string
     {
-		$this->ds_complemento = $ds_complemento;
-	}
+        return $this->nm_pessoa;
+    }
+
+    public function setNmPessoa(string $nm_pessoa)
+    {
+        $this->nm_pessoa = $nm_pessoa;
+    }
+
+    public function getNuCpf(): string
+    {
+        return $this->nu_cpf;
+    }
+
+    public function setNuCpf(string $nu_cpf)
+    {
+        $this->nu_cpf = $nu_cpf;
+    }
+
+    public function getNuRegistro(): string
+    {
+        return $this->nu_registro;
+    }
+
+    public function setNuRegistro(string $nu_registro)
+    {
+        $this->nu_registro = $nu_registro;
+    }
+
+    public function getDtNascimento(): string
+    {
+        return $this->dt_nascimento;
+    }
+
+    public function setDtNascimento(string $dt_nascimento)
+    {
+        $this->dt_nascimento = $dt_nascimento;
+    }
+
+    public function getSgOrgaoExpedidor(): string
+    {
+        return $this->sg_orgaoexpedidor;
+    }
+
+    public function setSgOrgaoExpedidor(string $sg_orgaoexpedidor)
+    {
+        $this->sg_orgaoexpedidor = $sg_orgaoexpedidor;
+    }
+
+    public function getNuCep(): string
+    {
+        return $this->nu_cep;
+    }
+
+    public function setNuCep(string $nu_cep)
+    {
+        $this->nu_cep = $nu_cep;
+    }
+
+    public function getNmRua(): string
+    {
+        return $this->nm_rua;
+    }
+
+    public function setNmRua(string $nm_rua)
+    {
+        $this->nm_rua = $nm_rua;
+    }
+
+    public function getNmBairro(): string
+    {
+        return $this->nm_bairro;
+    }
+
+    public function setNmBairro(string $nm_bairro)
+    {
+        $this->nm_bairro = $nm_bairro;
+    }
+
+    public function getDsComplemento(): string
+    {
+        return $this->ds_complemento;
+    }
+
+    public function setDsComplemento(string $ds_complemento)
+    {
+        $this->ds_complemento = $ds_complemento;
+    }
+
+    public function getNuEndereco(): string
+    {
+        return $this->nu_endereco;
+    }
+
+    public function setNuEndereco(string $nu_endereco)
+    {
+        $this->nu_endereco = $nu_endereco;
+    }
+
+    public function getNmEstado(): string
+    {
+        return $this->nm_estado;
+    }
+
+    public function setNmEstado(string $nm_estado)
+    {
+        $this->nm_estado = $nm_estado;
+    }
+
+    public function getNmCidade(): string
+    {
+        return $this->nm_cidade;
+    }
+
+    public function setNmCidade(string $nm_cidade)
+    {
+        $this->nm_cidade = $nm_cidade;
+    }
 }
-
