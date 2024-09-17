ce57a3248d4187b72ef5c8fb76d5b9ed94d21812
JulyellenS <julyellensantos1818@gmail.com>
2024-09-16 23:22:18 -0300

    funções de teste

--- app/control/test.php
+++ app/control/test.php
@@ -4,13 +4,17 @@ require_once __DIR__ . '../../service/PessoaService.php';
 
 $service = new PessoaService();
 
-$obter = $service->obterPessoa(1);
+// $obter = $service->obterPessoa(1);
 
-if(isset($obter) && !empty($obter))
-{
-    dump(['pessoa' => $obter]);
-}
-else
-{
-    dump('Não foi possivel obter resultado');
-}
+// if(isset($obter) && !empty($obter))
+// {
+//     dump(['pessoa' => $obter]);
+// }
+// else
+// {
+//     dump('Não foi possivel obter resultado');
+// }
+
+$pessoa = new Pessoa('1','Ana','12345678910','1234567890','1995-10-19','SSP-BA','41820-770','Rua ABC','Centro','Casa','470','Bahia (BA)','Salvador');
+
+$service->atualizarPessoa($pessoa);
