9fb9a4e8c50f4897a87bbc7be79d45a6d68d958f
JulyellenS <julyellensantos1818@gmail.com>
2024-09-16 23:16:26 -0300

    Adição de novas rotas

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
 require_once __DIR__ . '/app/control/PessoaList.php';
+require_once __DIR__ . '/app/control/PessoaForm.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'listar';
 
-$controller = new PessoaList();
+$pessoaList = new PessoaList();
+$pessoaForm = new PessoaForm();
 
-switch ($action) {
+switch ($action)
+{
+    case 'cadastrar':
+        $pessoaForm->cadastrar();
+        break;
     case 'editar':
-        $controller->editar();
+        $pessoaList->editar();
         break;
     case 'excluir':
-        $controller->excluir();
+        $pessoaList->excluir();
+        break;
+    case 'listar':
+        $pessoaList->listar();
         break;
     default:
-        $controller->listar();
+        $pessoaList->listar();
         break;
 }
