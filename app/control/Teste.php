f9b591dd590c38558191bf23f5b540330c3e7e92
JulyellenS <julyellensantos1818@gmail.com>
2024-09-21 04:28:48 +0000

    Adicionando mais testes

--- app/control/Teste.php
+++ app/control/Teste.php
@@ -2,28 +2,31 @@
 namespace App\Control;
 
 use App\Service\PessoaService;
+use App\Service\LoginService;
 use App\Model\Pessoa;
 class Teste
 {
-    private $service;
+    private $pessoaService;
+    private $loginService;
 
     public function __construct()
     {
-        $this->service = new PessoaService();
+        $this->pessoaService = new PessoaService();
+        $this->loginService = new LoginService();
     }
 
     public function excluirPessoa()
     {
-        $this->service->excluirPessoa(1);
+        $this->pessoaService->excluirPessoa(1);
     }
     
     public function obterPessoa()
     {
-        $obter = $this->service->obterPessoa(1);
+        $pessoa = $this->pessoaService->obterPessoa(1);
         
-        if(isset($obter) && !empty($obter))
+        if(isset($pessoa) && !empty($pessoa))
         {
-            dump(['pessoa' => $obter]);
+            dump(['pessoa' => $pessoa]);
         }
         else
         {
@@ -34,6 +37,14 @@ class Teste
     public function atualizarPessoa()
     {
         $pessoa = new Pessoa('1','Ana','12345678910','1234567890','1995-10-19','SSP-BA','41820-770','Rua ABC','Centro','Casa','470','Bahia (BA)','Salvador');
-        $this->service->atualizarPessoa($pessoa);
+        $this->pessoaService->atualizarPessoa($pessoa);
+    }
+
+    public function entrar()
+    {
+        $email = 'email@teste.com';
+        $senha = 'senha-aqui';
+
+        dump($this->loginService->entrar($email, $senha));
     }
 }
